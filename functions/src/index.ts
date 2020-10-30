39049336b470489f2baf0a9d5bfe4e70da783c5c
Sophie Koonin <sophiekoonin@monzo.com>
2020-10-30 19:34:16 +0000

    Fix types

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from 'firebase-functions';
-import express from 'express'
+import * as express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.send("hello world!")
 })
 // // Start writing Firebase Functions
