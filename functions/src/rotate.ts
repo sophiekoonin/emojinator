1935150085c4eab4174db49a74fe6a8c590a9b19
Sophie Koonin <sophiekoonin@monzo.com>
2020-10-30 19:31:06 +0000

    Comment out unused imports

--- functions/src/rotate.ts
+++ functions/src/rotate.ts
@@ -1,7 +1,7 @@
-const gm = require('gm').subClass({imageMagick: true});
-const fs = require('fs');
-const {promisify} = require('util');
-const path = require('path');
+// const gm = require('gm').subClass({imageMagick: true});
+// const fs = require('fs');
+// const {promisify} = require('util');
+// const path = require('path');
 import * as functions from 'firebase-functions';
 
 export const rotate = functions.https.onRequest((request, response) => {
