281e74e3de7a7ae1d24f61a7a78b7743f959026e
Sophie Koonin <sophiekoonin@monzo.com>
2020-10-31 14:06:40 +0000

    Get rotation working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
 import * as express from 'express'
 // @ts-ignore
 import * as expressParser from 'express-multipart-file-parser'
@@ -8,12 +8,12 @@ const app = express()
 app.use(expressParser)
 
 app.get('/thing', (req: express.Request, res: express.Response) => {
-  res.send("hello thing!")
+  res.send('hello thing!')
 })
 
 app.post('/rotate', rotate)
 app.get('/', (req: express.Request, res: express.Response) => {
-  return res.send("hello world!")
+  return res.send('hello world!')
 })
 
 exports.tools = functions.https.onRequest(app)
--- functions/src/rotate.ts
+++ functions/src/rotate.ts
@@ -1,39 +1,43 @@
 import * as express from 'express'
 import { createCanvas, Image } from 'canvas'
-const GIFEncoder = require('gif-encoder-2');
+const GIFEncoder = require('gif-encoder-2')
 
 // import * as functions from 'firebase-functions'
 
-const numSteps = 30;
-const rotationInDegrees = 360 / numSteps;
+const baseNumSteps = 30
 
 export default function rotate(req: express.Request, res: express.Response) {
-  // @ts-ignore 
+  // @ts-ignore-next-line
   const buffer = req.files[0].buffer
-  const image = new Image();
-  image.src = buffer;
-  const width = image.width + (image.width / 2);
-  const height = image.height + (image.width / 2);
-  const canvas = createCanvas(width, height);
-  const encoder = new GIFEncoder(width, height);
-  encoder.setTransparent(true);
-  encoder.start();
-  const ctx = canvas.getContext('2d');
-  
-  for (let i = 0; i < numSteps; i++) {
-    ctx.save();
-    ctx.clearRect(0, 0, width, height);
-  
-    ctx.translate(width / 2, height / 2); // set canvas context to centre
-    ctx.rotate(i * (rotationInDegrees * (Math.PI / 180)));
-    ctx.drawImage(image, -image.width / 2, -image.height / 2);
+  const { speed } = req.body || 1
+  const image = new Image()
+  image.src = buffer
+  const width = image.width + image.width / 2
+  const height = image.height + image.width / 2
+  const canvas = createCanvas(width, height)
+  const encoder = new GIFEncoder(width, height)
+  encoder.setTransparent(true)
+  encoder.start()
+  const ctx = canvas.getContext('2d')
 
-    encoder.addFrame(ctx);    
-    ctx.restore();
+  const numSteps = baseNumSteps * parseInt(speed)
+
+  const rotationInDegrees = 360 / numSteps
+
+  for (let i = 0; i < numSteps; i++) {
+    ctx.save()
+    ctx.clearRect(0, 0, width, height)
+
+    ctx.translate(width / 2, height / 2) // set canvas context to centre
+    ctx.rotate(i * (rotationInDegrees * (Math.PI / 180)))
+    ctx.drawImage(image, -image.width / 2, -image.height / 2)
+
+    encoder.addFrame(ctx)
+    ctx.restore()
   }
-  encoder.finish();
-  const outBuf = Buffer.from(encoder.out.getData());
+  encoder.finish()
+  const outBuf = Buffer.from(encoder.out.getData())
   res.set('Content-Type', 'image/gif')
- 
+
   res.send(outBuf)
 }
