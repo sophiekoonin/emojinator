46a5656390890ef9cd93b114c8a754ed4f9c0e9a
Sophie Koonin <sophiekoonin@monzo.com>
2020-10-31 13:56:34 +0000

    Get rotate handler working

--- functions/src/rotate.ts
+++ functions/src/rotate.ts
@@ -1,17 +1,39 @@
 import * as express from 'express'
+import { createCanvas, Image } from 'canvas'
+const GIFEncoder = require('gif-encoder-2');
 
 // import * as functions from 'firebase-functions'
 
-const gm = require('gm').subClass({imageMagick: true});
+const numSteps = 30;
+const rotationInDegrees = 360 / numSteps;
+
 export default function rotate(req: express.Request, res: express.Response) {
-  // @ts-ignore
-  const img = gm(req.files[0].buffer).command("convert").dispose("previous").in("-distort SRT \"1,%[fx:t*360/n]\"", "-duplicate 30").virtualPixel("transparent").set("delay", 8).loop(0).stream();
+  // @ts-ignore 
+  const buffer = req.files[0].buffer
+  const image = new Image();
+  image.src = buffer;
+  const width = image.width + (image.width / 2);
+  const height = image.height + (image.width / 2);
+  const canvas = createCanvas(width, height);
+  const encoder = new GIFEncoder(width, height);
+  encoder.setTransparent(true);
+  encoder.start();
+  const ctx = canvas.getContext('2d');
+  
+  for (let i = 0; i < numSteps; i++) {
+    ctx.save();
+    ctx.clearRect(0, 0, width, height);
+  
+    ctx.translate(width / 2, height / 2); // set canvas context to centre
+    ctx.rotate(i * (rotationInDegrees * (Math.PI / 180)));
+    ctx.drawImage(image, -image.width / 2, -image.height / 2);
 
-  // .toBuffer('GIF',function (err, buffer) {
-  //   if (err) res.status(500).send(err);
-  //   res.contentType('image/gif');
-  //   res.send(buffer)
-  // })
-
-  res.send(img)
+    encoder.addFrame(ctx);    
+    ctx.restore();
+  }
+  encoder.finish();
+  const outBuf = Buffer.from(encoder.out.getData());
+  res.set('Content-Type', 'image/gif')
+ 
+  res.send(outBuf)
 }
