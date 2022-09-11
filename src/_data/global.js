d5338f584c676e96c604933fa9f829c5a7d3b31f
Sophie Koonin <sophie@localghost.dev>
2022-09-11 15:33:32 +0100

    Fix CSS

--- src/_data/global.js
+++ src/_data/global.js
@@ -1,9 +1,13 @@
+const md5 = require("md5")
+const path = require("path")
+const fs = require("fs")
+
 module.exports = {
   baseUrl: process.env.BASE_URL || "https://emojinator.fun",
-  random() {
-    const segment = () => {
-      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1)
-    }
-    return `${segment()}-${segment()}-${segment()}`
+  cssFile() {
+    const scssDir = path.join(__dirname, "..", "scss")
+    const cssFile = fs.readFileSync(path.join(scssDir, "style.scss"))
+    const hash = md5(cssFile).slice(0, 8)
+    return `/css/style.${hash}.css`
   },
 }
